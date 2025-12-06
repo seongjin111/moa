13a034adf26c4906cd5e6e4d1d895466b1665b30
dsh38 <devsedah@gmail.com>
2025-12-07 01:21:45 +0900

    Clean all

--- mobility/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("route/", views.route_page, name="route_page"),
-    path("api/route/", views.route_to_stop, name="api_route_to_stop"),
-]
